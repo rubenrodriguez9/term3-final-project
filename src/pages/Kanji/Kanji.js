5d0d455b5ca094462a6461103a0824635193be6d
Ruben Rodriguez <ruben.rodriguez@codeimmersives.com>
2021-04-14 22:39:59 -0600

    Search functionality working

--- src/pages/Kanji/Kanji.js
+++ src/pages/Kanji/Kanji.js
@@ -16,26 +16,54 @@ const Kanji = (props) => {
     }, [])
 
     const [toggleModal, setToggleModal] = useState(false)
-    const searchKanji = useRef()
-    const handleKanjiAPI = async () => {
+    const [search, setSearch] = useState('')
+    const [errorToggle, setErrorToggle] = useState(false)
+    const [flashCards, setFlashCards] = useState([])
+
+    const handleKanjiAPI = async () => {      
+
+        setErrorToggle(false)
 
 
-
-        let response = await axios.get(`https://kanjialive-api.p.rapidapi.com/api/public/search/${searchKanji}`,{
-    "headers": {
-      "x-rapidapi-key": process.env.REACT_APP_KANJIAPIKEY,
-      "x-rapidapi-host": "kanjialive-api.p.rapidapi.com"
+        try {
+          let response = await axios.get(`https://kanjialive-api.p.rapidapi.com/api/public/search/${search}`,{
+          "headers": {
+            "x-rapidapi-key": "49e55a8007mshe7f1dd2f9efaf5dp1e5f32jsnfb100af21a58",
+            "x-rapidapi-host": "kanjialive-api.p.rapidapi.com"
+          }})
+          console.log(response);
+          if(response.data.length === 0){
+            setErrorToggle(true)
+          }
+          setFlashCards(response.data)
+        } catch (error) {
+          
+        }
+       
     }
-  }
-  
-  )
-    }
 
+    const addKanji = async (kanji) => {
+
+        try {
+            let response = await axios.get(`https://kanjialive-api.p.rapidapi.com/api/public/kanji/${kanji}`,{
+            "headers": {
+              "x-rapidapi-key": "49e55a8007mshe7f1dd2f9efaf5dp1e5f32jsnfb100af21a58",
+              "x-rapidapi-host": "kanjialive-api.p.rapidapi.com"
+            }})
+            console.log(response);
+            // if(response.data.length === 0){
+            //   setErrorToggle(true)
+            // }
+            // setFlashCards(response.data)
+          } catch (error) {
+            
+          };
+    }
     return (
         <div className="kanji-container-main">
 
             <div className="add-button" >
-            <button  onClick={() => setToggleModal(true)} class="button is-dark">+ Add Kanji</button>
+            <button  onClick={() => setToggleModal(!toggleModal)} class="button is-dark">+ Add Kanji</button>
             </div>
 
             <div className="kanji-nav-bar" >
@@ -51,12 +79,20 @@ const Kanji = (props) => {
               <div class="modal-background"></div>
               <div class="  modal-content">
               <div style={{backgroundColor: 'white'}} >
-                  <h2>Deck Name</h2>
-              <input ref={searchKanji} onChange={() => console.log(searchKanji.current.value)} class="input is-info"type="text" placeholder="Info input"/>
-              <button class="button is-primary">Submit</button> 
+
+              {errorToggle? <p style={{color: 'red'}} >Kanji does not exist or word contains multiple kanji</p>: null}
+      <div style={{alignItems: 'center'}} >
+      {flashCards.map((item) => {
+          return <p onClick={() => addKanji(item.kanji.character)} style={{fontSize: "50px"}} >{item.kanji.character}</p>
+        })}
+      </div>
+              <input class="input" value={search} onChange={(e) => setSearch(e.target.value)} type="text" placeholder="Text input"/>
+
+              <button onClick={handleKanjiAPI} class="button is-primary">Submit</button> 
               </div>
               </div>
-                <button  class="modal-close is-large" aria-label="close" style={{backgroundColor: 'red',}}></button>
+                <button  class="modal-close is-large" aria-label="close"  onClick={() => setToggleModal(!toggleModal)} style={{backgroundColor: 'red',}}></button>
+                
               </div> :
               null}
 
